c0cd8d09cdf863e23fa4a7542ff933cc7c5863b6
Paul Leflon <leflon.paul@gmail.com>
2025-01-23 22:07:18 +0100

    fix(env): use env variable to perform middleware redirections
    Using nextUrl on production failed because of the reverse proxy

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ const protectedRoutes = ['/entries', '/profile'];
 
 export default async function middleware(req: NextRequest) {
 	const path = req.nextUrl.pathname;
+	const base = process.env.BASE_URL;
 	const isProtectedRoute = protectedRoutes.some((route) => path.startsWith(route));
 
 	// 3. Decrypt the session from the cookie
@@ -15,18 +16,18 @@ export default async function middleware(req: NextRequest) {
 	try {
 		session = cookie ? await decrypt(cookie) : null;
 	} catch (_) {
-		return NextResponse.redirect(new URL('/logout', req.nextUrl));
+		return NextResponse.redirect(new URL('/logout', base));
 	}
 
 	const isLoggedIn = typeof session?.userId === 'number';
 
 	if (path.startsWith('/login') && isLoggedIn) {
-		return NextResponse.redirect(new URL('/entries/daily', req.nextUrl));
+		return NextResponse.redirect(new URL('/entries/daily', base));
 	}
 
 	// 5. Redirect to /login if the user is not authenticated
 	if (isProtectedRoute && !isLoggedIn) {
-		return NextResponse.redirect(new URL('/login', req.nextUrl));
+		return NextResponse.redirect(new URL('/login', base));
 	}
 
 	return NextResponse.next();
